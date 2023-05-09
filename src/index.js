edcabcf4f6fc899bc66cfc9360f69b8882e3e452
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-05-09 17:29:28 +0300

    add styles

---
+++ src/index.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const tabsItem = document.querySelectorAll('.tabs__btn-item');
+const tabsContent = document.querySelectorAll('.tabs__content-item');
+
+tabsItem.forEach(item => {
+  item.addEventListener('click', onClickBtn);
+});
+
+function onClickBtn(e) {
+  const tabTarget = e.currentTarget;
+  const button = tabTarget.dataset.button;
+
+  tabsItem.forEach(item => {
+    item.classList.remove('tabs__btn-item--active');
+  });
+
+  tabsContent.forEach(item => {
+    item.classList.remove('tabs__content-item--active');
+  });
+
+  tabTarget.classList.add('tabs__btn-item--active');
+  document
+    .querySelector(`#${button}`)
+    .classList.add('tabs__content-item--active');
+}
