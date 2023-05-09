412adaa9ba29f1ecfc6b4b5a4d6320e32c67f5c7
28Rostyk <107403659+28Rostyk@users.noreply.github.com>
2023-05-09 14:34:50 +0000

    Deploying to gh-pages from @ 28Rostyk/Oculus@edcabcf4f6fc899bc66cfc9360f69b8882e3e452 🚀

---
+++ index.a9235ad1.js
@@ -0,0 +1,2 @@
+const t=document.querySelectorAll(".tabs__btn-item"),e=document.querySelectorAll(".tabs__content-item");function c(c){const a=c.currentTarget,s=a.dataset.button;t.forEach((t=>{t.classList.remove("tabs__btn-item--active")})),e.forEach((t=>{t.classList.remove("tabs__content-item--active")})),a.classList.add("tabs__btn-item--active"),document.querySelector(`#${s}`).classList.add("tabs__content-item--active")}t.forEach((t=>{t.addEventListener("click",c)}));
+//# sourceMappingURL=index.a9235ad1.js.map
